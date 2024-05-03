7d8ec25d62bec8d0320bcd4d3dca38fef9fb34f7
GrzesiekJaremko <gergjaremko@gmail.com>
2024-05-03 13:54:54 +0100

    implemented some php

---
+++ site/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Workout Tracker</title>
+    </head>
+    <body>
+        <nav class="navbar">
+        <ul>
+            <li><a href="signup.html">Create New Account</a></li>
+            <li><a href="login.html">Log in</a></li>
+        </ul>
+        </nav>
---
+++ site/index.php
@@ -0,0 +1,6 @@
+<?php
+    include_once 'header.php';
+?>
+        <p>Who's gonna carry the boats...</p>
+    </body>
+</html>
