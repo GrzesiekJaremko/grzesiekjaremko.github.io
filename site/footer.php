e27d47090ecebc1d5f7b353d7395025dd9dc1413
GrzesiekJaremko <166860033+GrzesiekJaremko@users.noreply.github.com>
2025-01-13 18:54:45 +0000

    Delete site directory

--- site/footer.php
+++
@@ -1,5 +0,0 @@
-</body>
-<footer>
-    <p>this website was made by grzesiek jaremko</p>
-</footer>
-</html>
--- site/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once 'header.php';
-?>
-        <p>Who's gonna carry the boats...</p>
-
-<?php
-    include_once 'footer.php';
-?>
